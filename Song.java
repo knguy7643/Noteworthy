6d65583883716500717d9dba39409231f558f9c1
Austin Mould <47369147+AustinMould@users.noreply.github.com>
2021-04-25 14:51:20 -0500

    Add files via upload

---
+++ Song.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.scene.image.Image;
+
+public class Song {
+
+	public String name;
+	
+	public String artist;
+	
+	public int runtime;
+	
+	public String genre;
+	
+	public Image album;
+	
+	public Image sheetMusic;
+}
